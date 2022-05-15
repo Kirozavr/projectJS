42144bcb6b572a051b87f3d641b8aeb968e52270
Kirill <kirozavr@gmail.com>
2022-05-15 19:24:24 +0300

    First JS script after first lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,16 @@
-"use strict";
-const obj = {
-    name: 'Kirill',
-    age: 23,
-    home: 'Moscow'
+const number0fFilmes = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+// Поставили унарный плюс перед prompt, чтобы преобразовать ответ из строки в число;
+const personalMovieDB = {
+    count: number0fFilmes,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-console.log(obj.name);
+const a = prompt('Один из последних просмотренных фильмов?', ''), 
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько вы его оцените?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
