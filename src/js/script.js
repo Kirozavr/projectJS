e8996d05789575eef6efb2151fdca9058c8f6045
Kirill <kirozavr@gmail.com>
2022-05-22 20:04:36 +0300

    Commit after 3 homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
-const number0fFilmes = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-// Поставили унарный плюс перед prompt, чтобы преобразовать ответ из строки в число;
+"use strict";
+let number0fFilmes;
+// Функция узнает количество просмотренных фильмов.
+function start() {
+    number0fFilmes = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    // isNan - проверяет, что пользователь ввел число. Не должно быть True;
+    while (number0fFilmes == '' || number0fFilmes == null || isNaN(number0fFilmes)) {
+        number0fFilmes = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    }
+}
+// Вызов функции;
+start();
+// База данных.
 const personalMovieDB = {
     count: number0fFilmes,
     movies: {},
@@ -7,10 +18,44 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// Спрашивает последний фильм и его оценку.
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько вы его оцените?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+// Определяет уровень киноманства.
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+detectPersonalLevel();
+// Проверяет приватная ли база данных.
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+// Функция спрашивает про жанры и записывает их в базу.
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}?`);
+        personalMovieDB.genres.push(genre);
+    }
+}
+writeYourGenres();
 console.log(personalMovieDB);
